e40e7e3e670e41d5569a318f088a9ba6e8ff4709
VinnyVanGogh <133192356+VinnyVanGogh@users.noreply.github.com>
2024-10-01 11:52:27 -0700

    update to v1.0.5 <--- add html option, still need to figure out json later

--- vinmap/utils/html_utils.py
+++ vinmap/utils/html_utils.py
@@ -3,21 +3,6 @@
 import subprocess
 from pathlib import Path
 
-# def generate_html_report(output_file):
-#     home = str(Path.home())
-#     root_path = str(Path(__file__).parent.parent)
-#     html_path = root_path + '/scan-results/' + 'html'
-#     output_rm_path = output_file.split('/')[-1]
-#     output_file = home + '/NMAP/' + output_rm_path
-#     no_ext = output_rm_path.split('.')[0]
-#     html_file = no_ext + '.html'
-#     html_file = Path(html_path + '/' + html_file)
-#     print('Generating HTML report...', html_file)
-#     html_report = subprocess.run(['xsltproc', output_file, '-o', html_file])
-#     print(html_report)
-#     return html_file
-
-# redone cleaner
 def generate_html_report(output_file):
     home = str(Path.home())
     output_file = str(output_file)
